2059a60f4e4078a29e993846e901adb16f12d6a5
jpaz932 <juanpa.aristis@gmail.com>
2025-03-25 17:05:22 -0500

    Remove unused Request import from auth routes

--- routes/auth.php
+++ routes/auth.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\Api\LoginController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [LoginController::class, 'login']);
